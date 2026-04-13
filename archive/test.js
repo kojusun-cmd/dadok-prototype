9a6940fbdfc7debc5a2bd503c6357c3dd8bc66d5
kojusun-cmd <kojusun@gmail.com>
2026-04-13 17:58:34 +0900

    chore: remove archive and bat files

--- archive/test.js
+++
@@ -1,7 +0,0 @@
-
-const partner = { name: "A's Shop", region: "Seoul", place: "Gangnam", id: "P1", reviews: 10, rating: 5, massage: "Thai", age: "20s", image: "img.jpg" };
-const badgeHtml = '';
-const str = \<div class=\"card\" onclick=\"openProfile('\', '\ ¡¤ \', '\', \, \, '\', '\', '\', '\')\">
-\;
-console.log(str);
-
